3ee6f2e7ad4a0dba88913d42ce757076c05da2dd
Bill Mårtensson <bill@magictechnology.se>
2023-11-02 13:32:09 +0100

    pia12

---
+++ Pia12iosv1tors/DocodeView.swift
@@ -0,0 +1,18 @@
+//
+//  DocodeView.swift
+//  Pia12iosv1tors
+//
+//  Created by BillU on 2023-11-02.
+//
+
+import SwiftUI
+
+struct DocodeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DocodeView()
+}
